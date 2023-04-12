a3ab3fb15476a26269d905b885db0347cfb48704
Michał Masiulanis <m.masiulanis@gmail.com>
2023-04-12 21:44:44 +0200

    Modal click

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,15 +2,17 @@ import { Component } from 'react';
 
 export class Modal extends Component {
   componentDidMount() {
+    window.addEventListener('mousedown', this.handleClose);
     window.addEventListener('keydown', this.handleClose);
   }
 
   componentWillUnmount() {
+    window.removeEventListener('mousedown', this.handleClose);
     window.removeEventListener('keydown', this.handleClose);
   }
 
   handleClose = event => {
-    if (event.code === 'Escape') {
+    if (event.code === 'Escape' || 'click') {
       return this.props.onClose();
     }
   };
